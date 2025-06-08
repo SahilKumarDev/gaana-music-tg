586bc39123f9a1ce663c0dd8870a84c623956cd5
sahilkumardev <sahilkumar710@outlook.com>
2025-06-08 11:26:00 +0530

    Update START_IMG_URL to new image link

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg"
+    "START_IMG_URL", "https://envs.sh/aIP.jpg/IMG20250608353.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
